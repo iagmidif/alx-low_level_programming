58d636bc2f8b0ed94be094469b0395b2e6e34947
iagmidif <tariq86hamid@gmail.com>
2022-03-14 19:52:17 +0300

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * A Program that prints a string followed by a new line to the screen
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
