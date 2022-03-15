64a799d6158b3dc171dc6191e2eb977dfa8b768a
iagmidif <tariq86hamid@gmail.com>
2022-03-15 20:33:46 +0300

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: variable to be used
+ *
+ * Return: 1 if c is a letter
+ * 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c > 64 && c < 91) || (c > 96 && c < 123))
+		return (1);
+	return (0);
+}
