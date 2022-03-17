ec810d7051191d07801ec312cabf1182b1a8e143
iagmidif <tariq86hamid@gmail.com>
2022-03-17 20:54:01 +0300

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,12 +11,12 @@ void times_table(void)
 {
 	int a, i, j;
 
-	for (a = 0; a < 8; a++)
-		printf("0, ");
+	for (a = 0; a < 9; a++)
+		printf("0,  ");
 	puts("0");
 	for (i = 1; i < 10; i++)
 	{
-		printf("0,  ");
+		printf("0, ");
 		for (j = 1; j < 10; j++)
 		{
 			if (i * j / 10 == 0)
