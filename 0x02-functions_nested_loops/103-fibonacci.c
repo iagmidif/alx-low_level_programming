755756029a07bf63d6686da89079916288a06183
iagmidif <tariq86hamid@gmail.com>
2022-03-21 08:35:28 +0300

    task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * By considering the terms in the Fibonacci sequence whose
+ * values do not exceed 4,000,000, write a program that finds
+ * and prints the sum of the even-valued terms, followed by a new line.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long long int a, b, tmp, sum;
+
+	a = 1;
+	b = 2;
+	while (b < 4000000)
+	{
+		sum += a;
+		tmp = a + b;
+		a = b;
+		b = tmp;
+	}
+	printf("%llu\n", sum);
+	return (0);
+}
