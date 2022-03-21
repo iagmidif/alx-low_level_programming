c226b2936e40d2cc9ddd36f3ebb185690ec13aaf
iagmidif <tariq86hamid@gmail.com>
2022-03-21 08:56:18 +0300

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -14,10 +14,10 @@ int main(void)
 	a = 1;
 	b = 2;
 	tmp = 0;
-	for (i = 0; i < 98; i++)
+	for (i = 0; i < 97; i++)
 	{
 		tmp = a + b;
-		if (i == 97)
+		if (i == 96)
 			printf("%lu\n", b);
 		else if (i == 0)
 			printf("%lu, %lu, ", a, b);
