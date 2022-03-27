d75593a8ec42747966e1e4b62ac76fd0703cc43e
iagmidif <tariq86hamid@gmail.com>
2022-03-27 17:44:06 +0300

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: address of the string
+ *
+ * Return: length of *s
+ */
+int _strlen(char *s)
+{
+	int i, length;
+
+	i = 0;
+	length = 0;
+	while (*(s + i) != '\0')
+	{
+		length += 1;
+		i += 1;
+	}
+	return (length);
+}
+
+/**
+ * puts_half - prints half of a string, followed by a new line.
+ * print the second half of the string
+ * @str: the address of the string
+ *
+ * Return: void, nth
+ */
+void puts_half(char *str)
+{
+	int strLength, i;
+
+	strLength = _strlen(str);
+	if (strLength % 2 == 0)
+	{
+		for (i = strLength / 2; i < strLength; i++)
+			_putchar(*(str + i));
+	}
+	else
+	{
+		for (i = (strLength - 1) / 2; i < strLength; i++)
+			_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
+
