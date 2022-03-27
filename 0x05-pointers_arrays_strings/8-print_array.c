6bde643d8d18435164aed36ee8b221f4e51635b6
iagmidif <tariq86hamid@gmail.com>
2022-03-27 18:04:29 +0300

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == n - 1)
+			printf("%d\n", *(a + i));
+		else
+			printf("%d, ", *(a + i));
+	}
+}
