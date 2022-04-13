e6a982e92f7bad78bf1c16215d7575800099ec9d
iagmidif <tariq86hamid@gmail.com>
2022-04-13 19:05:15 +0300

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,11 +11,13 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i, len1, len2;
+	int i, len1 = 0, len2 = 0;
 	char *a;
 
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+	if (s1 != NULL)
+		len1 = strlen(s1);
+	if (s2 != NULL)
+		len2 = strlen(s2);
 
 	a = malloc(len1 + len2 + 1);
 	if (a == NULL)
@@ -27,6 +29,7 @@ char *str_concat(char *s1, char *s2)
 	for (i = len1; i < (len1 + len2); i++)
 		if (s2[i - len1] != '\0')
 			a[i] = s2[i - len1];
+	a[len1 + len2] = '\0';
 
 	return (a);
 }
