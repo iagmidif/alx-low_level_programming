515aa1d5f6cd80a1e3411cc6c82860058590fb63
iagmidif <tariq86hamid@gmail.com>
2022-04-16 17:15:30 +0300

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - my first structure
+ * @name: pointer to the name of the dog
+ * @age: age of the dog
+ * @owner: pointer to the name of the dog
+ *
+ * Description: A structure that represents a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
