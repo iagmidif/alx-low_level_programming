bb3d25d0612cf88d8c3c06ed442e655e48d102c2
iagmidif <tariq86hamid@gmail.com>
2022-04-16 12:33:35 +0300

    update

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,7 +9,8 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *arr;
+	unsigned int i;
+	char *arr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -17,6 +18,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	arr = malloc(size * nmemb);
 	if (arr == NULL)
 		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		arr[i] = 0;
 
 	return (arr);
 }
