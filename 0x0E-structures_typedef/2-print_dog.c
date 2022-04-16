d21a3db34d6dc5905df52dc1b0d5f4785cf27c21
iagmidif <tariq86hamid@gmail.com>
2022-04-16 17:36:48 +0300

    task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+
+#define NIL "(nil)"
+/**
+ * print_dog - prints a struct dog
+ * @d: pointer to the instance
+ *
+ * Return: void, nth
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	printf("Name: %s\n", ((d->name) ? (d->name) : NIL));
+	if (d->age)
+		printf("Age: %f\n", d->age);
+	else
+		printf("Age: %s\n", NIL);
+	printf("Owner : %s\n", ((d->owner) ? (d->owner) : NIL));
+}
