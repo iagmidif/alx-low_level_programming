ff94dad6d8b50cc1478ee1be49db611ca4e5056e
iagmidif <tariq86hamid@gmail.com>
2022-04-17 16:17:26 +0300

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+/**
+ * print_name - prints a name using a callback function
+ * @name: pointer to the name
+ * @f: pointer to the printing function to be used
+ *
+ * Return: void, nth
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
