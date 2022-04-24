06eb9d1c718f821eb99862b2707d8850e24501e8
iagmidif <tariq86hamid@gmail.com>
2022-04-24 21:38:53 +0300

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * before_main - prints a statement before the main function is executed
+ *
+ * Return: void, nth
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
