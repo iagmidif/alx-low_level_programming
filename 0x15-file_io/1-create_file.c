77778a0cf27a0473a41fd30c099d0c761f404ab7
iagmidif <tariq86hamid@gmail.com>
2022-05-03 09:58:07 +0300

    task 1 update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-		return (0);
+		return (1);
 
 	len = strlen(text_content);
 	n = write(fd, text_content, len);
