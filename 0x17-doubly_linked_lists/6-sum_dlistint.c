eed0e64a28af3f91ff09a704e8d84ab2f0793781
Tariq <tariq86hamid@gmail.com>
2022-06-10 10:05:25 -0700

    task 6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns the sum of all the data of a dlistint_t linked list.
+ * @head: pointer to the DLL
+ *
+ * Return: the sum of the DLL's data, if the DLL is empty return 0
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *h = head;
+	int sum = 0;
+
+	while (h->prev != NULL)
+		h = h->prev;
+	while (h != NULL)
+	{
+		sum += h->n;
+		h = h->next;
+	}
+	return (sum);
+}
