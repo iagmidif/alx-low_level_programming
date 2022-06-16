68afd12b27109c724297ab218d1bc472144c8fb0
Tariq <tariq86hamid@gmail.com>
2022-06-16 02:03:56 -0700

    task 3

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,59 @@
+/**
+ * add - adds two numbers
+ * @a: first int
+ * @b: second integer
+ *
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two numbers
+ * @a: first int
+ * @b: second integer
+ *
+ * Return: a - b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two numbers
+ * @a: first int
+ * @b: second integer
+ *
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two numbers
+ * @a: first int
+ * @b: second integer
+ *
+ * Return: a / b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modulous of two numbers
+ * @a: first int
+ * @b: second integer
+ *
+ * Return: a % b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
